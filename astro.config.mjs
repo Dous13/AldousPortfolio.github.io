df795fe502ce31ee4ecc9718ece2ab3fe312bd04
Zidous <jarredjeneo@gmail.com>
2026-06-03 11:46:04 +0800

    metadata change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://aldousportfolio.github.io',
+  site: 'https://Dous13.github.io',
   base: '/AldousPortfolio.github.io',
-  output: 'static',
 });

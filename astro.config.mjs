26decc4ccee239c2ca065dfdea901f192bcd8c36
Zidous <jarredjeneo@gmail.com>
2026-06-03 11:18:10 +0800

    deploy update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-
-   site: 'https://Aldous.com'
+  site: 'https://aldousportfolio.github.io',
+  base: '/AldousPortfolio.github.io',
+  output: 'static',
 });
